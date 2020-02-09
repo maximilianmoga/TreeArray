e72a01cf22470dad795f2ada2085c7e1562e66c9
Maximilian Moga <maximilianmoga@gmail.com>
2020-02-09 14:53:34 -0800

    chart

--- results.js
+++ results.js
@@ -1,13 +1,13 @@
-const results = JSON.parse(localStorage.getItem('__votes'));
+//const results = JSON.parse(localStorage.getItem('__votes'));
+//console.log(result)
 
+const votes = ['2', '5', '7', '11'];
+const labels = ['red', 'blue', 'brown', 'orange', 'purple', 'yellow'];
 
-const votes = [];
-const labels = [];
-
-results.forEach(item => {
-    votes.push(item.votes);
-    labels.push(item.id);
-});
+// results.forEach(item => {
+//     votes.push(item.votes);
+//     labels.push(item.id);
+// });
 
 const ctx = document.getElementById('results').getContext('2d');
 
